d2c7570640b5c5042ae343fcae176b23c8da7e3f
Tesfahun B Abose <93301043+Tesfagit@users.noreply.github.com>
2022-01-28 21:54:23 -0500

    some code

---
+++ our_javaScript.js
@@ -0,0 +1,14 @@
+function incr() {
+  let num = 1;
+  document.getElementById("tesfa").innerHTML = num++;
+}
+function takeBack() {
+  let num = 1;
+  return num;
+}
+function para() {
+  document.getElementById("tesfa-para").innerHTML =
+    " Hey Fre, the top Nav is sticky when you scroll up. And like, share," +
+    "and dislike buttons increment, when you click; take a look at it. See" +
+    "it on both phone and pc. Thank you! ";
+}
